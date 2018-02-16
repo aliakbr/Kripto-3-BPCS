b5da6c8f4f2e0877487f562d8aedce8c5a23d024
ranggarmaste <ranggarmaste@gmail.com>
2018-02-16 14:47:14 +0700

    Fix main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ DECRYPT = 2
 
 img_input = ImageTk.PhotoImage(Image.open('gray.png'))
 img_output = ImageTk.PhotoImage(Image.open('gray.png'))
+filename = ''
 
 def open_image():
     global img_input, left_image
@@ -18,6 +19,41 @@ def open_image():
     img_input = ImageTk.PhotoImage(Image.open(filepath))
     left_image.configure(image=img_input)
     left_image.image = img_input
+    ## Read image matrix di sini ya Ali. Pake filepath
+
+def upload_file():
+    global filename, right_image, filename_label
+    filepath = filedialog.askopenfilename(initialdir = "/",title = "Select file")
+    # Add code to read file content
+    content = 'Ini isi filenya'
+
+    splits = filepath.split('/')
+    filename = splits[len(splits)-1]
+    right_image.configure(text=content)
+    right_image.configure(image=None)
+    right_image.image = None
+    filename_label.configure(text='Filename: ' + filename)
+
+def start():
+    if mode.get() == ENCRYPT:
+        # Kode encrypt
+        pass
+    else:
+        # Kode decrypt
+        pass
+
+def change_mode():
+    global left_label, middle_label, middle_button, right_button
+    if mode.get() == ENCRYPT:
+        left_label.configure(text="Original Image")
+        middle_label.configure(text="BPCS Image")
+        middle_button.configure(state=tk.NORMAL)
+        right_button.configure(text="Upload File")
+    else:
+        left_label.configure(text="BPCS Image")
+        middle_label.configure(text="-")
+        middle_button.configure(state=tk.DISABLED)
+        right_button.configure(text="Save Extracted")
 
 mode = tk.IntVar()
 mode.set(ENCRYPT)
@@ -32,15 +68,16 @@ title_frame.pack()
 
 # Main Frame
 main_frame = tk.Frame(root)
-panel_width = 400
-panel_height = 400
+panel_width = 300
+panel_height = 300
 
 # Left frame
 left_frame = tk.Frame(main_frame)
 left_label = tk.Label(left_frame,
                 text="Original Image",
                 fg = "dark green",
-                font = "Helvetica 14 bold").pack(pady=5)
+                font = "Helvetica 14 bold")
+left_label.pack(pady=5)
 left_image = tk.Label(left_frame,
                 image=img_input,
                 borderwidth=2,
@@ -50,7 +87,8 @@ left_image.pack(pady=5)
 left_button = tk.Button(left_frame,
                 text="Upload",
                 fg="blue",
-                command=open_image).pack(pady=5)
+                command=open_image)
+left_button.pack(pady=5)
 left_frame.pack(side=tk.LEFT, padx=10)
 
 # Middle frame
@@ -58,16 +96,19 @@ middle_frame = tk.Frame(main_frame)
 middle_label = tk.Label(middle_frame,
                 text="BPCS Image",
                 fg = "dark green",
-                font = "Helvetica 14 bold").pack(pady=5)
+                font = "Helvetica 14 bold")
+middle_label.pack(pady=5)
 middle_image = tk.Label(middle_frame,
                 image=img_output,
                 borderwidth=2,
                 relief="groove",
-                width=panel_width, height=panel_height).pack(pady=5)
+                width=panel_width, height=panel_height)
+middle_image.pack(pady=5)
 middle_button = tk.Button(middle_frame,
                 text="Save",
                 fg="blue",
-                command=open_image).pack(pady=5)
+                command=open_image)
+middle_button.pack(pady=5)
 middle_frame.pack(side=tk.LEFT, padx=10)
 
 # Right frame
@@ -75,20 +116,23 @@ right_frame = tk.Frame(main_frame)
 right_label = tk.Label(right_frame,
                 text="Message (In Bytes)",
                 fg = "dark green",
-                font = "Helvetica 14 bold").pack(pady=5)
+                font = "Helvetica 14 bold")
+right_label.pack(pady=5)
 right_image = tk.Label(right_frame,
-                image=img_input,
                 borderwidth=2,
                 relief="groove",
-                width=panel_width, height=panel_height).pack(pady=5)
+                width=40, height=20)
+right_image.pack(pady=5)
 filename_label = tk.Label(right_frame,
-                text="Filename: mimi.exe",
+                text="Filename: -",
                 fg = "black",
-                font = "Helvetica 12").pack(pady=5)
+                font = "Helvetica 10")
+filename_label.pack(pady=5)
 right_button = tk.Button(right_frame,
                 text="Upload File",
                 fg="blue",
-                command=open_image).pack(pady=5)
+                command=upload_file)
+right_button.pack(pady=5)
 right_frame.pack(side=tk.LEFT, padx=10)
 
 main_frame.pack()
@@ -106,11 +150,13 @@ tk.Radiobutton(mode_frame,
               text="Encrypt",
               padx=20,
               variable=mode,
+              command=change_mode,
               value=ENCRYPT).pack(anchor=tk.W)
 tk.Radiobutton(mode_frame,
               text="Decrypt",
               padx=20,
               variable=mode,
+              command=change_mode,
               value=DECRYPT).pack(anchor=tk.W)
 mode_frame.pack(side=tk.LEFT, pady=5)
 
@@ -128,7 +174,7 @@ input_frame.pack(side=tk.LEFT, padx=5, pady=5)
 start_button = tk.Button(bottom_frame,
                 text="Start",
                 fg="blue",
-                command=open_image).pack(side=tk.RIGHT, pady=5)
+                command=start).pack(side=tk.RIGHT, pady=5)
 
 bottom_frame.pack()
 
